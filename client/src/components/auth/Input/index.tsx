94d5a026a48cca03871b96d2d3651cbff13058b9
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-01-21 18:22:42 +0200

    added show-hide password

--- client/src/components/auth/Input/index.tsx
+++ client/src/components/auth/Input/index.tsx
@@ -1,7 +1,7 @@
 import { upperCaseFirstLetter } from '@/utils/upperCaseFirstLetter';
 import { Feather } from '@expo/vector-icons';
-import { Dispatch, FC, SetStateAction } from 'react';
-import { View, TextInput, Text } from 'react-native';
+import { Dispatch, FC, SetStateAction, useState } from 'react';
+import { View, TextInput, Text, TouchableOpacity } from 'react-native';
 
 type IProps = {
   value: string;
@@ -13,6 +13,12 @@ type IProps = {
 };
 
 export const Input: FC<IProps> = ({ value, onChange, placeholder, icon, error, isSecured }) => {
+  const [isPasswordShown, setIsPasswordShown] = useState<boolean>(true);
+
+  const toggleShowPassword = (): void => {
+    setIsPasswordShown(!isPasswordShown);
+  };
+
   return (
     <View className="relative">
       <Feather
@@ -22,13 +28,21 @@ export const Input: FC<IProps> = ({ value, onChange, placeholder, icon, error, i
         style={{ position: 'absolute', top: 12, left: 12, zIndex: 100 }}
       />
       <TextInput
-        secureTextEntry={isSecured}
+        secureTextEntry={isPasswordShown}
         className="pl-11 py-2 w-64 mb-1 text-white rounded bg-gray-800"
         placeholderTextColor="#ccc"
         placeholder={placeholder}
         value={value}
         onChangeText={(text) => onChange(text)}
       />
+      {isSecured && (
+        <TouchableOpacity
+          style={{ position: 'absolute', top: 12, right: 12, zIndex: 100 }}
+          onPress={toggleShowPassword}
+        >
+          <Feather name={isPasswordShown ? 'eye' : 'eye-off'} size={20} color="#ccc" />
+        </TouchableOpacity>
+      )}
       <Text className="w-64 text-xs text-red-500 mb-3">{error && upperCaseFirstLetter(error)}</Text>
     </View>
   );
