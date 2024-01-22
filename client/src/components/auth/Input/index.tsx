61f134ded0e6a878b86998f19dd50330c80105d2
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-01-22 10:14:31 +0200

    refactored Input

--- client/src/components/auth/Input/index.tsx
+++ client/src/components/auth/Input/index.tsx
@@ -1,5 +1,5 @@
-import { upperCaseFirstLetter } from '@/utils/upperCaseFirstLetter';
-import { Feather } from '@expo/vector-icons';
+import { upperCaseFirstLetter } from '@/utils';
+import { Feather} from '@expo/vector-icons';
 import { Dispatch, FC, SetStateAction, useState } from 'react';
 import { View, TextInput, Text, TouchableOpacity } from 'react-native';
 
@@ -7,7 +7,7 @@ type IProps = {
   value: string;
   onChange: Dispatch<SetStateAction<string>>;
   placeholder: string;
-  icon: 'mail' | 'lock' | 'key';
+  icon: 'mail' | 'lock' | 'key' | 'search';
   error: string;
   isSecured?: boolean;
 };
@@ -29,7 +29,7 @@ export const Input: FC<IProps> = ({ value, onChange, placeholder, icon, error, i
       />
       <TextInput
         secureTextEntry={isPasswordShown}
-        className="pl-11 py-2 w-64 mb-1 text-white rounded bg-gray-800"
+        className="pl-11 py-2 w-full mb-1 text-white rounded bg-gray-800"
         placeholderTextColor="#ccc"
         placeholder={placeholder}
         value={value}
@@ -43,7 +43,7 @@ export const Input: FC<IProps> = ({ value, onChange, placeholder, icon, error, i
           <Feather name={isPasswordShown ? 'eye' : 'eye-off'} size={20} color="#ccc" />
         </TouchableOpacity>
       )}
-      <Text className="w-64 text-xs text-red-500 mb-3">{error && upperCaseFirstLetter(error)}</Text>
+      <Text className="text-xs text-red-500 mb-3">{error && upperCaseFirstLetter(error)}</Text>
     </View>
   );
 };
