14927339b3cdf06ca0041f9949c87bbf14ebbd17
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-01-22 09:09:57 +0200

    fixed refreshToken mutation

--- client/src/apollo/mutations/refreshToken.ts
+++ client/src/apollo/mutations/refreshToken.ts
@@ -3,7 +3,8 @@ import { gql } from '@apollo/client';
 export const REFRESH_TOKEN_MUTATION = gql`
   mutation RefreshAccess($refreshTokenDto: refreshTokenDto!) {
     refreshAccess(refreshTokenDto: $refreshTokenDto) {
-      accessToken
+      accessToken,
+      refreshToken
     }
   }
 `;
