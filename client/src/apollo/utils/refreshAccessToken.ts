3fcfcf3993dc2a9cf6170731cbd0f0dc98eaf0f6
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-01-22 09:13:54 +0200

    refactored refreshAccessToken

--- client/src/apollo/utils/refreshAccessToken.ts
+++ client/src/apollo/utils/refreshAccessToken.ts
@@ -4,29 +4,31 @@ import * as SecureStore from 'expo-secure-store';
 
 export const refreshAccessToken = async (
   apolloClient: ApolloClient<NormalizedCacheObject>,
-): Promise<string> => {
+): Promise<string | undefined> => {
   try {
-    const tokens = (await SecureStore.getItemAsync('tokens')) || '';
-    const { refreshToken } = JSON.parse(tokens);
+    const tokens = await SecureStore.getItemAsync('tokens');
+    if (tokens) {
+      const { refreshToken } = JSON.parse(tokens);
 
-    const refreshResolverResponse = await apolloClient.mutate({
-      mutation: REFRESH_TOKEN_MUTATION,
-      variables: {
-        refreshTokenDto: { refreshToken },
-      },
-    });
+      const refreshResolverResponse = await apolloClient.mutate({
+        mutation: REFRESH_TOKEN_MUTATION,
+        variables: {
+          refreshTokenDto: { refreshToken },
+        },
+      });
 
-    const accessToken = refreshResolverResponse.data?.refreshAccess.accessToken;
-    const newRefreshToken = refreshResolverResponse.data?.refreshAccess.refreshToken;
+      const accessToken = refreshResolverResponse.data?.refreshAccess.accessToken;
+      const newRefreshToken = refreshResolverResponse.data?.refreshAccess.refreshToken;
 
-    if (accessToken) {
-      await SecureStore.setItemAsync(
-        'tokens',
-        JSON.stringify({ accessToken, refreshToken: newRefreshToken }),
-      );
-      return accessToken;
-    } else {
-      throw new Error('Access token not found in the response');
+      if (accessToken) {
+        await SecureStore.setItemAsync(
+          'tokens',
+          JSON.stringify({ accessToken, refreshToken: newRefreshToken }),
+        );
+        return accessToken;
+      } else {
+        throw new Error('Access token not found in the response');
+      }
     }
   } catch (err) {
     throw err;
