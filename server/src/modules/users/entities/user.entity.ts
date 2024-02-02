7f5cfc765eb86dbe28eb5c38e5a335a1ccd7d1fe
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-02-02 15:53:55 +0200

    refactored user entity

--- server/src/modules/users/entities/user.entity.ts
+++ server/src/modules/users/entities/user.entity.ts
@@ -1,22 +1,17 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
-import { Field } from '@nestjs/graphql';
 import { IUser } from '../interfaces';
 
 @Entity({ name: 'users' })
 export class User implements IUser {
-  @Field(() => String)
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Field(() => String)
   @Column({ unique: true })
   email: string;
 
-  @Field(() => String)
   @Column()
   passwordHash: string;
 
-  @Field(() => String, { nullable: true })
   @Column({ nullable: true })
   refreshToken: string | null;
 }
