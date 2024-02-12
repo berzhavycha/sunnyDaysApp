5a8fe769aeaabf6d9c82cbdc21402d7569530e74
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-02-12 19:38:13 +0200

    removed extraneous dotenv call in database module

--- server/src/modules/database/configs/index.ts
+++ server/src/modules/database/configs/index.ts
@@ -1,8 +1,5 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
-import { config as dotenvConfig } from 'dotenv';
-
-dotenvConfig();
 
 export const typeOrmConfigOptions = {
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
