8a68a3a22f14dcfa537df8463af21aecc756f2ca
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-02-14 11:54:51 +0200

    provided better env names for geodb

--- client/envValidation.js
+++ client/envValidation.js
@@ -2,11 +2,11 @@ const Joi = require("joi");
 
 const envSchema = Joi.object({
     GRAPHQL_BASE_URL: Joi.string().required(),
-    FETCH_CITY_AUTOCOMPLETE_AMOUNT: Joi.number().default(5),
+    FETCH_CITY_AUTOCOMPLETE_LIMIT: Joi.number().default(5),
     FETCH_CITY_AUTOCOMPLETE_SORT: Joi.string().default("population"),
     FETCH_CITY_AUTOCOMPLETE_OFFSET: Joi.number().default(0),
     MAX_WEATHER_CITIES_AMOUNT: Joi.number().default(10),
-    MAX_FORECAST_DAYS: Joi.number().default(10),
+    MAX_FORECAST_DAYS: Joi.number().default(3),
     WEATHER_FORECAST_CACHE_MINUTES_TIME: Joi.number().default(30),
     PASSWORD_MIN_LENGTH: Joi.number().default(8),
 })
--- client/src/components/weatherForecast/WeatherForecastDisplay/components/WeatherHeader/components/WeatherHeaderDown/utils/getCitiesQueryVariables.ts
+++ client/src/components/weatherForecast/WeatherForecastDisplay/components/WeatherHeader/components/WeatherHeaderDown/utils/getCitiesQueryVariables.ts
@@ -5,5 +5,5 @@ export const getCitiesQueryVariables = (city: string): CitiesQueryVariables => (
   prefix: city,
   sort: Env.FETCH_CITY_AUTOCOMPLETE_SORT,
   offset: Env.FETCH_CITY_AUTOCOMPLETE_OFFSET,
-  limit: Env.FETCH_CITY_AUTOCOMPLETE_AMOUNT,
+  limit: Env.FETCH_CITY_AUTOCOMPLETE_LIMIT,
 });
