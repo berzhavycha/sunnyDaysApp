dff1b9c846f5e8455198318ffc40089c51e0a44a
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-03-05 20:39:50 +0200

    added .env

--- web/src/components/common/Spinner/index.tsx
+++ web/src/components/common/Spinner/index.tsx
@@ -1,9 +1,7 @@
-import { Spinner as NextSpinner } from "@chakra-ui/spinner";
-
 export const Spinner = (): JSX.Element => {
     return (
         <div className="fixed top-0 left-0 w-full h-screen bg-gray-900 flex justify-center items-center z-100">
-            <NextSpinner size="lg" className="w-24 h-24 text-blue-500" />
+            Loading...
         </div>
     );
 };
