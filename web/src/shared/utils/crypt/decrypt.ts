acf84dfbb123b379413a5f56830a3dc59262fbd1
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-03-11 21:30:54 +0200

    refactored crypt

--- web/src/shared/utils/crypt/decrypt.ts
+++ web/src/shared/utils/crypt/decrypt.ts
@@ -2,6 +2,5 @@ import * as CryptoJS from 'crypto-js';
 
 export const decrypt = (hash: string, key: string): string => {
     const decryptedBytes = CryptoJS.AES.decrypt(hash, key);
-    const decryptedToken = decryptedBytes.toString(CryptoJS.enc.Utf8);
-    return decryptedToken;
+    return decryptedBytes.toString(CryptoJS.enc.Utf8);
 }
--- web/src/shared/utils/crypt/encrypt.ts
+++ web/src/shared/utils/crypt/encrypt.ts
@@ -1,6 +1,5 @@
 import * as CryptoJS from 'crypto-js';
 
 export const encrypt = (value: string, key: string): string => {
-    const encryptedToken = CryptoJS.AES.encrypt(value, key).toString();
-    return encryptedToken;
+    return CryptoJS.AES.encrypt(value, key).toString();
 }
