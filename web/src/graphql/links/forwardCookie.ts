5c71d94df1c78a67959381eca11f66a8ba367680
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-03-12 15:20:18 +0200

    fixed warinings in the console

--- web/src/graphql/links/forwardCookie.ts
+++ web/src/graphql/links/forwardCookie.ts
@@ -2,7 +2,6 @@ import { ApolloLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 export const forwardCookieLink = (name: string, value: string): ApolloLink => {
-    console.log(`${name}=${value}`)
     return setContext(async () => {
         return {
             headers: {
--- web/src/graphql/typePolicies/userCitiesWeather.ts
+++ web/src/graphql/typePolicies/userCitiesWeather.ts
@@ -63,14 +63,12 @@ export const userCitiesWeather: FieldPolicy<PaginationResult> = {
       return !context.readField<boolean>('_deleted', edge);
     });
 
-    console.log("MERGE", filteredMergedEdge.map(edge => context.readField<string>('city', edge)))
     return {
       edges: filteredMergedEdge,
       paginationInfo: mergedPaginationInfo,
     };
   },
   read(existing, context) {
-    console.log("READ", existing?.edges.map(edge => context.readField<string>('city', edge)))
     return (
       existing && {
         edges: existing.edges.slice(
