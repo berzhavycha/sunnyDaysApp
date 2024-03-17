68645d7448d437c1859390d9ad60612e085acffc
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-03-17 19:18:58 +0200

    fixed unapplied metadata on root layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,14 @@
+import type { Metadata } from 'next';
+
 import { MainLayout } from '@/layouts';
 
 import './globals.css';
+
+
+export const metadata: Metadata = {
+  title: 'Sunny Days',
+  description: 'Sunny Days App',
+};
 
 export default function RootLayout({
   children,
