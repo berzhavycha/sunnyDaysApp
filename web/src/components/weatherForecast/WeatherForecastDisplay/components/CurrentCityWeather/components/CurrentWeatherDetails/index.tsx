466f64399473c75e226f6cabdfc0b11d1c658882
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-03-27 13:43:36 +0200

    fix error related to different client and server className in currentWeatherDetails

--- web/src/components/weatherForecast/WeatherForecastDisplay/components/CurrentCityWeather/components/CurrentWeatherDetails/index.tsx
+++ web/src/components/weatherForecast/WeatherForecastDisplay/components/CurrentCityWeather/components/CurrentWeatherDetails/index.tsx
@@ -8,7 +8,7 @@ import {
   useWeatherPagination,
   usePaginationPrefetch,
 } from '@/hooks';
-import { IS_CLIENT, START_PAGE_NUMBER } from '@/shared';
+import { START_PAGE_NUMBER } from '@/shared';
 import { Spinner, NoData, DeleteButton } from '@/components/common';
 import { TodayWeatherInfo, Forecast } from './components';
 import { useCurrentWeatherTime } from './hooks';
@@ -41,7 +41,7 @@ export const CurrentWeatherDetails: FC<Props> = ({ currentWeatherRef }) => {
   return (
     <div
       ref={currentWeatherRef}
-      className={`${isVisible && IS_CLIENT ? 'flex' : 'hidden'} fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex-col gap-5 bg-blue-800 rounded-3xl p-5 ${isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'} transition-opacity duration-300 z-10 w-4/5 md:flex md:left-8 md:top-8 md:transform md:translate-x-0 md:translate-y-0 md:w-[45%] md:h-[92%] lg:w-2/5 xl:w-1/3 2xl:w-1/4 md:flex`}
+      className={`${isVisible ? 'flex' : 'hidden'} fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex-col gap-5 bg-blue-800 rounded-3xl p-5 ${isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'} transition-opacity duration-300 z-10 w-4/5 md:flex md:left-8 md:top-8 md:transform md:translate-x-0 md:translate-y-0 md:w-[45%] md:h-[92%] lg:w-2/5 xl:w-1/3 2xl:w-1/4 md:flex`}
     >
       {loading ? (
         <Spinner />
