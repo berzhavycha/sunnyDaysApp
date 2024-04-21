bb07d92797ae56a9d9a6d60787dcd24740b6893d
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-04-21 19:48:55 +0300

    got rid of useEffect in useCurrentWeatherTime

--- web/src/components/weatherForecast/WeatherForecastDisplay/components/CurrentCityWeather/components/CurrentWeatherDetails/hooks/useCurrentWeatherTime/index.tsx
+++ web/src/components/weatherForecast/WeatherForecastDisplay/components/CurrentCityWeather/components/CurrentWeatherDetails/hooks/useCurrentWeatherTime/index.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from 'react';
-
 import { CurrentCityWeatherInfoState } from '@/context';
 import { daysOfWeek, getTimeFormat } from '@/shared';
 
@@ -11,22 +9,17 @@ type HookReturn = {
 export const useCurrentWeatherTime = (
   currentCityWeatherInfo: CurrentCityWeatherInfoState | undefined,
 ): HookReturn => {
-  const [dayOfWeek, setDayOfWeek] = useState<string>('');
-  const [time, setTime] = useState<string>('');
+  let dayOfWeek: string = ''
+  let time: string = ''
 
-  useEffect(() => {
-    if (currentCityWeatherInfo?.info?.dayOfWeek) {
-      setDayOfWeek(currentCityWeatherInfo?.info.dayOfWeek);
-      setTime('');
-    } else {
-      const dateInstance = new Date(currentCityWeatherInfo?.info?.time ?? '');
-      const dayOfWeek = daysOfWeek[dateInstance.getDay()];
-      setDayOfWeek(dayOfWeek);
-      setTime(
-        `${getTimeFormat(dateInstance.getHours())}:${getTimeFormat(dateInstance.getMinutes())}`,
-      );
-    }
-  }, [currentCityWeatherInfo]);
+  if (currentCityWeatherInfo?.info?.dayOfWeek) {
+    dayOfWeek = currentCityWeatherInfo.info.dayOfWeek
+    time = ''
+  } else {
+    const dateInstance = new Date(currentCityWeatherInfo?.info?.time ?? '');
+    dayOfWeek = daysOfWeek[dateInstance.getDay()];
+    time = `${getTimeFormat(dateInstance.getHours())}:${getTimeFormat(dateInstance.getMinutes())}`
+  }
 
   return { dayOfWeek, time };
 };
