4a322606bc5acec55cb3e970f20d9e62ef5c2cb5
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-04-22 12:31:29 +0300

    added TodaysForecastButton component

--- web/src/components/weatherForecast/WeatherForecastDisplay/components/CurrentCityWeather/components/CurrentWeatherDetails/components/Forecast/index.tsx
+++ web/src/components/weatherForecast/WeatherForecastDisplay/components/CurrentCityWeather/components/CurrentWeatherDetails/components/Forecast/index.tsx
@@ -7,6 +7,7 @@ import { WeatherForecastDays } from '@/shared';
 
 import './style/index.css';
 import { InteractiveForecastItems } from '../InteractiveForecastItem';
+import { TodaysForecastButton } from '../TodaysForecastButton';
 
 type Props = {
   info: WeatherForecastDays[];
@@ -24,14 +25,7 @@ export const Forecast: FC<Props> = memo(({ info }) => {
     <div className="bg-blue-600 rounded-3xl py-2 px-2 h-full sm:pt-4 sm:py-2 sm:px-3">
       <div className="flex items-center justify-between h-8 mb-1 sm:mb-2 md:mb-4 md:max-lg:mb-1">
         <p className="text-white font-bold text-sm pl-3 md:text-lg">Forecast</p>
-        {!isTodayCurrentWeather && (
-          <button
-            onClick={onTodayCurrentWeather}
-            className="bg-blue-800 text-white text-xs px-4 py-1 rounded-xl mr-2 md:text-sm md:px-2 lg:px-4"
-          >
-            Get Today`s Forecast
-          </button>
-        )}
+        {!isTodayCurrentWeather && <TodaysForecastButton onClick={onTodayCurrentWeather} />}
       </div>
       <CustomFlatList
         data={info}
