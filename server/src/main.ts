8ec3e9025f23700ebb78e81d5bf5c3be6a47fecd
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-05-13 23:07:20 +0300

    got rid of magic string for cors origin

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,16 +7,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: configService.get<string>('CORS_ORIGIN'),
     credentials: true,
   });
 
-  const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
 
   await app.listen(port);
--- server/src/modules/config/validation/app.ts
+++ server/src/modules/config/validation/app.ts
@@ -2,6 +2,7 @@ import * as Joi from 'joi';
 
 export const appValidationSchema = Joi.object({
   NODE_ENV: Joi.string().default('development'),
+  CORS_ORIGIN: Joi.string().required(),
   LOGIN_FIELD: Joi.string().default('email'),
   PORT: Joi.number().default(4000),
   COOKIE_EXPIRATION_DAYS_TIME: Joi.number().default(30),
